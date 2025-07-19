f95782b9a00ef4e8714114372232b0a179ba99ff
Narmin <mamedova.nermin002@gmail.com>
2025-07-19 21:15:59 +0400

    Initial commit

---
+++ src/BankManagementSystem.java
@@ -0,0 +1,39 @@
+
+import java.util.Scanner;
+
+public class BankManagementSystem {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int balance = 0;
+        while(true) {
+            System.out.println("==== Bank Menu ====");
+            System.out.println("1.Deposit");
+            System.out.println("2.Withdraw");
+            System.out.println("3.Check Balance");
+            System.out.println("4.Exit");
+            System.out.println("Enter your choice: ");
+            int choice = scanner.nextInt();
+            if (choice == 1) {
+                System.out.println("Enter amount to deposit: ");
+                int moneyToDeposit = scanner.nextInt();
+                balance+=moneyToDeposit;
+                System.out.println("Money deposited.");
+            }
+            else if (choice == 2) {
+                System.out.println("Enter amount to withdraw: ");
+                int moneyToWithdraw = scanner.nextInt();
+                balance-=moneyToWithdraw;
+                System.out.println("Money withdrawn.");
+            }
+            else if (choice == 3) {
+                System.out.println("Your balance is: " + balance);
+            }
+            else if (choice == 4) {
+               break;
+            }
+            else{
+                System.out.println("Invalid choice");
+            }
+        }
+    }
+}
